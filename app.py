6c6c358c5d1c81f18f0629348f6ae43e9dc3e927
asluchych <anatolblr@gmail.com>
2022-06-03 22:51:47 +0200

    Add Update

--- app.py
+++ app.py
@@ -41,6 +41,21 @@ def delete(id):
     except:
         return "Task could not be deleted"
 
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form["content"]
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Updating the task failed"
+        
+    else:
+        return render_template('update.html', task = task)
+
 if __name__ == "__main__":
     app.run(debug=True)
 
